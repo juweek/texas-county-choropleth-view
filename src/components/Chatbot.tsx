92099e0c87ec5eb70e9ab96c6604544c5427f1e2
juweek adolphe <juweek.adolphe@gmail.com>
2025-05-14 18:12:08 -0400

    added the correct timestamp

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -9,7 +9,7 @@ interface Message {
 export default function Chatbot() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [inputValue, setInputValue] = useState('');
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
   // Get API URL from environment variable or use localhost for development
@@ -79,7 +79,7 @@ export default function Chatbot() {
             setIsCollapsed(!isCollapsed);
           }}
         >
-          {isCollapsed ? (
+          {!isCollapsed ? (
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
             </svg>
